1d84627832278feccc11ac9d4df13d9beed5323e
Michael Houmann <mhoumann87@gmail.com>
2024-11-12 20:47:45 +0100

    Update index, add components, update tailwind.config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Urbanist", system-UI, sans-serif'],
+      },
+      container: {
+        padding: "2rem",
+        center: true,
+      },
+      colors: {
+        background: "#F3D4A8",
+        text: "#180E0F",
+        accent: "#64748B",
+      },
+    },
+  },
+  plugins: [],
+};
